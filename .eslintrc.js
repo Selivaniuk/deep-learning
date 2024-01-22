d9297481804cfa07df1d9656b805966dcc1968bf
Evgeny <selivanukev@gmail.com>
2024-01-22 06:02:22 +0500

    add i18next plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,63 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended",
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'standard-with-typescript',
+		'plugin:react/recommended',
 		'plugin:prettier/recommended',
 		'plugin:import/recommended',
-		"plugin:import/typescript",
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-	"settings": {
-		"import/parsers": {
-			"@typescript-eslint/parser": ['.js', 'jsx', ".ts", ".tsx"]
-		  },
-		  "import/resolver": {
-			"typescript": true,
-			"node": true,
-		  }
-	  },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-		"import/order": [
-			"error",
+		'plugin:import/typescript',
+		'plugin:i18next/recommended',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.js', 'jsx', '.ts', '.tsx'],
+		},
+		'import/resolver': {
+			typescript: true,
+			node: true,
+		},
+	},
+	plugins: ['react', 'i18next'],
+	rules: {
+		'import/order': [
+			'error',
 			{
-			  "groups": ["external", "builtin", "index", "sibling", "parent", "internal", "type"],
-			  "alphabetize": {
-				"order": "asc",
-				"caseInsensitive": true
-			  },
-			  "newlines-between": "always-and-inside-groups"
-			}
-		  ],
+				groups: [
+					'external',
+					'builtin',
+					'index',
+					'sibling',
+					'parent',
+					'internal',
+					'type',
+				],
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+				'newlines-between': 'always-and-inside-groups',
+			},
+		],
 		'react/react-in-jsx-scope': 0,
-		"import/named":0,
-    }
-}
+		'import/named': 0,
+		'i18next/no-literal-string': 2,
+	},
+};
--- src/pages/TestPage/ui/TestPage.tsx
+++ src/pages/TestPage/ui/TestPage.tsx
@@ -1,9 +1,12 @@
 import {type FC} from 'react';
+import {useTranslation} from 'react-i18next';
 
 const TestPage: FC = () => {
+	const {t} = useTranslation('translation');
+
 	return (
 		<div>
-			<h1>Test Page!</h1>
+			<h1>{t('test-page')}</h1>
 		</div>
 	);
 };
